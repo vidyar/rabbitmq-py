64a353d497ca7d78f529025d1eb76549db7e1525
Patrick Ellis <oneredoak@gmail.com>
2014-06-19 14:11:32 -0700

    Removal of Invalid Syntax

--- rabbit.py
+++ rabbit.py
@@ -17,7 +17,7 @@ class Rabbit():
         channel.queue_declare(queue='hello')
         print ' [*] Waiting for messages. To exit press CTRL+C'
         def callback(ch, method, properties, body):
-            @message = body
+            message = body
             print " [x] Received %r" % (body,)
         channel.basic_consume(callback, queue='hello', no_ack=True)
         channel.start_consuming()
